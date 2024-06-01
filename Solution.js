3014cde1096b4da05e689e0b699e7f1757d6fc23
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-01 11:59:42 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,17 @@
+
+/**
+ * @param {string} binaryInt
+ * @return {number}
+ */
+var numSteps = function (binaryInt) {
+    let carryOver = 0;
+    let transformationSteps = 0;
+
+    for (let bit = binaryInt.length - 1; bit > 0; --bit) {
+        let current = binaryInt.codePointAt(bit) - '0'.codePointAt(0);
+        transformationSteps += 1 + (current + carryOver) % 2;
+        carryOver = carryOver | current;
+    }
+
+    return transformationSteps + carryOver;
+};
